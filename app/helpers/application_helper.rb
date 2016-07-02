62595aa5ff2b3d16a914529dc27bba974ec9f2b3
Bennet11 <bennetveloya@gmail.com>
2016-07-02 04:35:13 -0700

    markdown successful

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def markdown_to_html(markdown)
+    render_options = { safe_links_only: true, prettify: true }
+    extensions = { autolink: true, highlight: true, quote: true }
+    renderer = Redcarpet::Render::HTML.new(render_options)
+    redcarpet = Redcarpet::Markdown.new(renderer, extensions)
+    (redcarpet.render markdown).html_safe
+  end
 end
